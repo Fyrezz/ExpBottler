5ddfab9e23c8fd26585514f62689092b1de35d2a
Fyrezz <edusanpaz98@gmail.com>
2019-04-05 23:12:29 +0200

    Update

--- src/main/java/net/fyrezz/me/expbottler/util/MagicExpBottle.java
+++ src/main/java/net/fyrezz/me/expbottler/util/MagicExpBottle.java
@@ -1,6 +1,5 @@
 package net.fyrezz.me.expbottler.util;
 
-import java.awt.List;
 import java.util.ArrayList;
 
 import org.bukkit.Material;
@@ -11,7 +10,7 @@ import net.fyrezz.me.expbottler.P;
 public class MagicExpBottle extends ItemStack {
 	
 	private int experience;
-	private List<String> lore = new ArrayList<String>();
+	private ArrayList<String> lore = new ArrayList<String>();
 	
 	public MagicExpBottle(int experience) {
 		this.experience = experience;
