9f0462c877389329b76f28059fb1cbbfe4f7268a
Fyrezz <eduardo.sanchez.paz@gmail.com>
2019-04-06 21:16:45 +0200

    Re added

---
+++ src/main/java/net/fyrezz/me/expbottler/CustomConfig.java
@@ -0,0 +1,37 @@
+package net.fyrezz.me.expbottler;
+
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class CustomConfig {
+	
+	private String fileName;
+	private File file;
+	private FileConfiguration config;
+	
+	public CustomConfig(String fileName) {
+		this.fileName = fileName;
+		this.file = new File(P.p.getDataFolder(), fileName);
+		this.config = YamlConfiguration.loadConfiguration(file);
+	}
+	
+	public FileConfiguration getConfig() {
+		if (config == null) {
+			reloadConfig();
+		}
+		return config;
+	}
+	
+	public void saveDefaultConfig() {
+		if (!file.exists()) {
+			P.p.saveResource(fileName, false);
+		}
+	}
+	
+	public void reloadConfig() {
+		config = YamlConfiguration.loadConfiguration(file);
+	}
+
+}
